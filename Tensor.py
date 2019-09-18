4e928675fba25bbe5bce36a54eea120b2d85be16
SXH <shenxuhui1992@gmail.com>
2019-09-18 23:00:54 +0800

    add test case for tensor class

--- Tensor.py
+++ Tensor.py
@@ -2,10 +2,10 @@ import numpy as np
 
 class Tensor (object):
     def __init__(self, data,
-                 auto_grad = False,
-                 creators = None,
-                 creation_op = None,
-                 id = None):
+                 auto_grad=False,
+                 creators=None,
+                 creation_op=None,
+                 id=None):
         self.data = np.array(data)
         self.auto_grad = auto_grad
         self.creators = creators
@@ -25,3 +25,15 @@ class Tensor (object):
     def __reptr__(self):
 
     def __str__(self):
+
+a = Tensor([1, 2, 3, 4, 5], autograd=True)
+b = Tensor([2, 2, 2, 2, 2], autograd=True)
+c = Tensor([5, 4, 3, 2, 1], autograd=True)
+
+d = a + b
+e = b + c
+f = d + e
+
+f.backward(Tensor(np.array([1, 1, 1, 1, 1])))
+
+print(b.grad.data == np.array([2, 2, 2, 2, 2]))
